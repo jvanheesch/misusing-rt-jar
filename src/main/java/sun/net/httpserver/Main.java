baf5cff4ecaaa4d486fc933055cc03debcae765b
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2018-10-18 20:36:35 +0200

    Fixed!

--- src/main/java/sun/net/httpserver/Main.java
+++ src/main/java/sun/net/httpserver/Main.java
@@ -2,6 +2,7 @@ package sun.net.httpserver;
 
 import java.io.IOException;
 
+// -Xbootclasspath/a:/home/joris/projects/examples/misusingrtjar/target/classes/
 public class Main {
     public static void main(String[] args) throws IOException {
         Class<ExchangeImpl> exchangeClass = ExchangeImpl.class;
