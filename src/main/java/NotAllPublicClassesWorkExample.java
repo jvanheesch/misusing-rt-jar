ac1e046967b7caecea6a52253479dae0014097ef
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2018-10-21 00:13:04 +0200

    NotAllPublicClassesWorkExample.

--- src/main/java/NotAllPublicClassesWorkExample.java
+++ src/main/java/NotAllPublicClassesWorkExample.java
@@ -5,6 +5,7 @@ public class NotAllPublicClassesWorkExample {
      * - warning on line sun.font.AttributeMap
      * - error on line sun.net.httpserver.AuthFilter
      * Explanation: https://stackoverflow.com/a/4070685/1939921
+     * Compilation succeeds if we add '-XDignore.symbol.file'.
      */
     public static void main(String[] args) {
         sun.font.AttributeMap test = new sun.font.AttributeMap(null);
