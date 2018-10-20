3d550d40410fc87be2b179367eb5b42e1c536f79
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2018-10-21 00:10:45 +0200

    NotAllPublicClassesWorkExample.

---
+++ src/main/java/NotAllPublicClassesWorkExample.java
@@ -0,0 +1,13 @@
+public class NotAllPublicClassesWorkExample {
+    /**
+     * [.../src/main/java] javac NotAllPublicClassesWorkExample.java
+     * result:
+     * - warning on line sun.font.AttributeMap
+     * - error on line sun.net.httpserver.AuthFilter
+     * Explanation: https://stackoverflow.com/a/4070685/1939921
+     */
+    public static void main(String[] args) {
+        sun.font.AttributeMap test = new sun.font.AttributeMap(null);
+        sun.net.httpserver.AuthFilter fail;
+    }
+}
